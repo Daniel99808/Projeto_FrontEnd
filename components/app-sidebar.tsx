c97d5fc6aff2f8eb9d1f84f6c26b48253a9e0028
Daniel99808 <danielfonsecamonteiro07@gmail.com>
2025-11-05 14:13:06 -0300

    feat: finaliza CRUD completo de categorias com UI

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -47,10 +47,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
             >
               <Link href="/painel">
-                <Pizza className="!size-5" />
+                <Pizza className="size-5!" />
                 <span className="text-base font-semibold">Delivery</span>
               </Link>
             </SidebarMenuButton>
