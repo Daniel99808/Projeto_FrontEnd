17bb183650def6664677cb1f514d7e2bec37bc0c
Daniel99808 <danielfonsecamonteiro07@gmail.com>
2025-11-19 14:04:17 -0300

    2. Página da Categoria - Adicionar campo foto aos modelos

---
+++ prisma/migrations/20251119165942_adicionar_foto_categorias_produtos/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Categorias" ADD COLUMN "foto" TEXT DEFAULT '/categorias/default.png';
+
+-- AlterTable
+ALTER TABLE "Produtos" ADD COLUMN "foto" TEXT DEFAULT '/produtos/default.png';
